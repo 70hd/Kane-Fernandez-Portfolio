5f23d89da50d21968953e73ad5339dca8d95c92b
70hd <kanehfernandez@gmail.com>
2025-10-10 19:21:03 -0700

    clerk

--- middleware.js
+++ middleware.js
@@ -12,16 +12,9 @@
 // };
 
 // middleware.js
+// middleware.js
 import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// If you need to customize, pass an options object to clerkMiddleware({...})
 export default clerkMiddleware();
-
-// Safe matcher: skip _next and any file with an extension
 export const config = {
-  matcher: [
-    "/((?!.*\\..*|_next).*)", // all app routes, excluding static files and Next internals
-    "/",                      // root
-    "/(api|trpc)(.*)",        // your API routes (only if you actually need auth here)
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
